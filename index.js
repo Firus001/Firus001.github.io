ccef1c1d93b249e2792be50f8013bd42f6682eeb
Firus001 <74003744+Firus001@users.noreply.github.com>
2021-11-23 00:07:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+document.getElementById("ej2").style.display="none";
+document.getElementById("ej3").style.display="none";
+document.getElementById("ej4").style.display="none";
+document.getElementById("ej5").style.display="none";
+document.getElementById("ej6").style.display="none";
+document.getElementById("ej7").style.display="none";
+document.getElementById("ej8").style.display="none";
+document.getElementById("ej9").style.display="none";
+function ocultar(){
+    document.getElementById("ej1").style.display="none";
+    document.getElementById("ej2").style.display="none";
+    document.getElementById("ej3").style.display="none";
+    document.getElementById("ej4").style.display="none";
+    document.getElementById("ej5").style.display="none";
+    document.getElementById("ej6").style.display="none";
+    document.getElementById("ej7").style.display="none";
+    document.getElementById("ej8").style.display="none";
+    document.getElementById("ej9").style.display="none";
+}
+
+
+function ej1_0(){
+    ocultar();
+    document.getElementById("ej1").style.display="flex";
+}
+function ej2(){
+    ocultar();
+    document.getElementById("ej2").style.display="flex";
+}
+function ej3(){
+    ocultar();
+    document.getElementById("ej3").style.display="flex";
+}
+function ej4(){
+    ocultar();
+    document.getElementById("ej4").style.display="flex";
+}
+function ej5(){
+    ocultar();
+    document.getElementById("ej5").style.display="flex";
+}
+function ej6(){
+    ocultar();
+    document.getElementById("ej6").style.display="flex";
+}
+function ej7(){
+    ocultar();
+    document.getElementById("ej7").style.display="flex";
+}
+function ej8(){
+    ocultar();
+    document.getElementById("ej8").style.display="flex";
+}
+function ej9(){
+    ocultar();
+    document.getElementById("ej9").style.display="flex";
+}
